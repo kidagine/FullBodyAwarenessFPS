c4a0154e7eadea66204aadc288374e6479bb0680
unknown <davidkalatzis@gmail.com>
2020-06-27 12:41:22 +0200

    Updated player ui functionality and minor additions.

---
+++ Assets/ScriptableObjects/GunSO.cs
@@ -0,0 +1,12 @@
+﻿
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "GunSO", menuName = "ScriptableObjects/GunSO")]
+public class GunSO : ScriptableObject
+{
+    public string gunName;
+    public Sprite gunIcon;
+    public int currentClipSize;
+    public int maxClipSize;
+    public int totalAmmoAmount;
+}
--- Assets/Scripts/UIScripts/PlayerGunUI.cs
+++ Assets/Scripts/UIScripts/PlayerGunUI.cs
@@ -3,9 +3,29 @@ using UnityEngine;
 
 public class PlayerGunUI : MonoBehaviour
 {
+    [SerializeField] private Animator _animator = default;
+    [SerializeField] private GameObject _gunCanvas = default;
     [SerializeField] private TextMeshProUGUI _currentClipText = default;
     [SerializeField] private TextMeshProUGUI _totalAmmoText = default;
+    [SerializeField] private TextMeshProUGUI _gunNameText = default;
 
+
+    public void SetGunUI(bool enable, GunSO gunSO = default)
+    {
+        _gunCanvas.SetActive(enable);
+        if (enable)
+        {
+            _animator.SetTrigger("ShowName");
+            _gunNameText.text = gunSO.gunName;
+            _currentClipText.text = gunSO.currentClipSize.ToString();
+            _totalAmmoText.text = gunSO.totalAmmoAmount.ToString();
+        }
+    }
+
+    public void SetGunName(string name)
+    {
+        _gunNameText.text = name;
+    }
 
     public void SetCurrentClip(int amount)
     {
